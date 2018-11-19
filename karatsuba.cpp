5c2999180b374baddde98ef9693f608152df83cb
zjh19960128 <zjh199601283492@gmail.com>
2018-11-18 21:14:13 -0500

    change

--- karatsuba.cpp
+++ karatsuba.cpp
@@ -11,7 +11,12 @@ size_t* transformtoint(std::string input){
 }
 std::string transformtostr(size_t* input,size_t m){
    std::string re="";
+   size_t sum=0;
    for(size_t i=0;i<m;i++){
+      sum=sum+input[i];
+      if(sum==0){
+         continue;
+      }
       re=re+std::to_string(input[i]);
    }
    return re;
@@ -20,6 +25,24 @@ void powerten(std::string& input,size_t nb){
    for(size_t i=0;i<nb;i++){
       input=input+'0';
    }
+}
+std::string add(std::string& one,std::string& two){
+   size_t sizeone=one.size();
+   size_t sizetwo=two.size();
+   size_t bigger=sizetwo>sizeone ? sizetwo : sizeone;
+   size_t smaller=sizetwo<sizeone ? sizetwo: sizeone;
+   size_t* digitone=transformtoint(one);
+   size_t* digittwo=transformtoint(two);
+   size_t* digitsum=new size_t [bigger+1];
+   for(size_t i=0;i<bigger+1;i++){
+      digitsum[i]=0;
+   }
+   /*currently we don't consider increase to one*/
+
+   delete [] digitone;
+   delete [] digittwo;
+   delete [] digitsum;
+   return transformtostr(digitsum,bigger+1);
 }
 size_t* prod(size_t* inputone,size_t m,size_t* inputtwo,size_t n){
    if(m<2 && n<2){
@@ -31,9 +54,5 @@ size_t* prod(size_t* inputone,size_t m,size_t* inputtwo,size_t n){
 int main(){
    std::string input1="3141592653589793238462643383279502884197169399375105820974944592";
    std::string input2="2718281828459045235360287471352662497757247093699959574966967627";
-   size_t* inputone=transformtoint(input1);
-   size_t* inputtwo=transformtoint(input2);
-   std::string temp=transformtostr(inputone,64);
-   powerten(temp,1);
-   std::cout<<temp<<std::endl;
+   std::cout<<add(input1,input2)<<std::endl;
 }
