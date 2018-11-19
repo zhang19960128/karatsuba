d130fc12c5e7289ad01952297153b48d59721989
zjh19960128 <zjh199601283492@gmail.com>
2018-11-18 20:20:55 -0500

    first

--- karatsuba.cpp
+++ karatsuba.cpp
@@ -17,10 +17,13 @@ std::string transformtostr(size_t* input,size_t m){
    return re;
 }
 void powerten(std::string& input,size_t nb){
-
+   for(size_t i=0;i<nb;i++){
+      input=input+'0';
+   }
 }
 size_t* prod(size_t* inputone,size_t m,size_t* inputtwo,size_t n){
-   if(m<2 || n<2){
+   if(m<2 && n<2){
+
    }
    else{
    }
@@ -30,5 +33,7 @@ int main(){
    std::string input2="2718281828459045235360287471352662497757247093699959574966967627";
    size_t* inputone=transformtoint(input1);
    size_t* inputtwo=transformtoint(input2);
-   std::cout<<transformtostr(inputone,64);
+   std::string temp=transformtostr(inputone,64);
+   powerten(temp,1);
+   std::cout<<temp<<std::endl;
 }
